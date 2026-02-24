e48a73fd23881be80a72c69a7d0373f306e1c1df
Hruaia <hruaitluangasailo20@gmail.com>
2026-02-25 01:32:00 +0530

    1

--- src/pages/SubjectsQuiz.jsx
+++ src/pages/SubjectsQuiz.jsx
@@ -10,64 +10,26 @@ export default function SubjectsQuiz() {
   const [subjectData, setSubjectData] = useState([]);
 
   // Mock data (simulates backend response)
-  useEffect(() => {
-    const mockSubjectData = [
-      {
-        id: 1,
-        img: "https://images.unsplash.com/photo-1513258496099-48168024aec0?w=600",
-        subject: "Subject Name",
-        teacher: "Teacher Name",
-      },
-      {
-        id: 2,
-        img: "https://images.unsplash.com/photo-1553877522-43269d4ea984?w=600",
-        subject: "Subject Name",
-        teacher: "Teacher Name",
-      },
-      {
-        id: 3,
-        img: "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f?w=600",
-        subject: "Subject Name",
-        teacher: "Teacher Name",
-      },
-      {
-        id: 4,
-        img: "https://images.unsplash.com/photo-1544717305-2782549b5136?w=600",
-        subject: "Subject Name",
-        teacher: "Teacher Name",
-      },
-      {
-        id: 5,
-        img: "https://images.unsplash.com/photo-1553877522-43269d4ea984?w=600",
-        subject: "Subject Name",
-        teacher: "Teacher Name",
-      },
-      {
-        id: 6,
-        img: "https://images.unsplash.com/photo-1454165205744-3b78555e5572?w=600",
-        subject: "Subject Name",
-        teacher: "Teacher Name",
-      },
-      {
-        id: 7,
-        img: "https://images.unsplash.com/photo-1454165205744-3b78555e5572?w=600",
-        subject: "Subject Name",
-        teacher: "Teacher Name",
-      },
-    ];
+ useEffect(() => {
+  const fetchSubjects = async () => {
+    try {
+      const res = await fetch("/api/student/quiz-subjects/", {
+        credentials: "include",
+      });
 
-    setSubjectData(mockSubjectData);
-  }, []);
+      if (!res.ok) throw new Error("Failed to fetch subjects");
 
-{/*   // example for backend //
+      const data = await res.json();
+      setSubjectData(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-  useEffect(() => {
-  fetch("/api/subjects-quiz")
-    .then((res) => res.json())
-    .then((data) => setSubjectData(data));
+  fetchSubjects();
 }, []);
 
-*/}
+
 
   return (
     <div className="subjectsPage">
